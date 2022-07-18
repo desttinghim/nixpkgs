e8b66a2cff92745e4ed3aca380a0edfade588b22
Jonathan Ringer <jonringer117@gmail.com>
2022-07-18 11:42:46 -0700

    python3Packages.rx: switch to pyproject, fix build

--- pkgs/development/python-modules/rx/default.nix
+++ pkgs/development/python-modules/rx/default.nix
@@ -1,9 +1,15 @@
-{ lib, fetchFromGitHub, buildPythonPackage, pythonOlder, nose }:
+{ lib, fetchFromGitHub, buildPythonPackage
+, pythonOlder
+, poetry-core
+, nose
+, typing-extensions
+}:
 
 buildPythonPackage rec {
   pname = "rx";
   version = "4.0.4";
   disabled = pythonOlder "3.6";
+  format = "pyproject";
 
   # There are no tests on the pypi source
   src = fetchFromGitHub {
@@ -12,6 +18,14 @@ buildPythonPackage rec {
     rev = "refs/tags/v${version}";
     sha256 = "sha256-W1qYNbYV6Roz1GJtP/vpoPD6KigWaaQOWe1R5DZHlUw=";
   };
+
+  nativeBuildInputs = [
+    poetry-core
+  ];
+
+  propagatedBuildInputs = [
+    typing-extensions
+  ];
 
   checkInputs = [ nose ];
 
